b0a7237c09e8d1d18d304f448a889bfb49110983
zubayerhimel <zhimel007@gmail.com>
2019-10-28 22:03:39 +0600

    initial commit, created home page

---
+++ assets/js/plugin.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.tooltipped').tooltip();
+    $('.sidenav').sidenav();
+    $('select').formSelect();
+    $('.dropdown-trigger').dropdown();
+    $('.tabs').tabs();
+});

1b720bd2db858deae69b15b84179767976bc5ece
Md Zinnatul Islam Morol <31995155+jinnatul@users.noreply.github.com>
2020-09-06 21:33:57 +0600

    Minified 11 files

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -1,10 +1,1 @@
-$(document).ready(function() {
-    $('.sidenav').sidenav();
-    $('.dropdown-trigger').dropdown();
-    $('.tabs').tabs();
-    $('.slider').slider();
-
-    $("#deletebtn").click(function(){
-        $("#deletediv").remove();
-    });
-});
+$(document).ready((function(){$(".sidenav").sidenav(),$(".dropdown-trigger").dropdown(),$(".tabs").tabs(),$(".slider").slider(),$("#deletebtn").click((function(){$("#deletediv").remove()}))}));
